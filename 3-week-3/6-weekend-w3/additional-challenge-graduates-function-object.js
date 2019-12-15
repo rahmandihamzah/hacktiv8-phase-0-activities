1e83de1d9b845f9cfdc029fc9cb2c1778079980f
rahmandihamzah <rahmandi.dev@gmail.com>
2019-12-15 22:32:17 +0700

    additional challenge w3d6

--- 3-week-3/6-weekend-w3/additional-challenge-graduates-function-object.js
+++ 3-week-3/6-weekend-w3/additional-challenge-graduates-function-object.js
@@ -19,6 +19,9 @@ BUAT DUA FUNCTION DIMANA FUNCTION PERTAMA OUTPUTNYA SEBAGAI BERIKUT:
   { classCode: 'B', passed: [], failed: [ 'Mike', 'Osass' ] },
   { classCode: 'C', passed: [ 'Budi', 'Yolo' ], failed: [ 'Arthur' ] },
 ]
+
+lolos : >= 70
+
 FUNCTION KEDUA OUTPUTNYA SEBAGAI BERIKUT:
 // {
 //   passed: [ 'John', 'Budi', 'Yolo' ],
@@ -30,3 +33,47 @@ FUNCTION KEDUA OUTPUTNYA SEBAGAI BERIKUT:
 //   }
 // }
 */
+
+function passedStudent(data){
+  let resultData = [];
+
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].score >= 70) {
+      for (let j = 0; j <= resultData.length; j++) {
+        if (resultData.length === 0) {
+          resultData.push({});
+          resultData[resultData.length - 1].classCode = data[i].classCode;
+          resultData[resultData.length - 1].passed = [];
+          resultData[resultData.length - 1].passed.push(data[i].name);
+          console.log(resultData[j].classCode);
+        } else if (data[i].classCode === resultData[j].classCode) {
+          resultData[resultData[j]].passed.push(data[i].name);
+        } else if (j === resultData.length) {
+          resultData.push({});
+          resultData[resultData.length - 1].classCode = data[i].classCode;
+          resultData[resultData.length - 1].passed = [];
+          resultData[resultData.length - 1].passed.push(data[i].name);
+        }
+      }
+    }
+  }
+
+  return resultData;
+}
+
+function passedStudentDetail(dataPassed){
+  let resultData = dataPassed;
+
+  return resultData;
+}
+
+console.log(passedStudentDetail(passedStudent([
+  { name: 'John', score: 80, classCode: 'A' },
+  { name: 'Mike', score: 60, classCode: 'B' },
+  { name: 'Budi', score: 70, classCode: 'C' },
+  { name: 'Siti', score: 50, classCode: 'A' },
+  { name: 'Aaron', score: 10, classCode: 'A' },
+  { name: 'Arthur', score: 10, classCode: 'C' },
+  { name: 'Osass', score: 10, classCode: 'B' },
+  { name: 'Yolo', score: 90, classCode: 'C' },
+])));
